b0c1ce89c99e3ca6f6bfa2536816a752aed528ec
vikei <vikei.dev@gmail.com>
2020-05-11 17:13:34 +0300

    Configure typeorm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions'
 import { AuthModule } from '../auth'
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -14,7 +17,16 @@ import { AuthModule } from '../auth'
         typePaths: ['./**/*.graphql'],
       })
     }),
-    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('PG_URL'),
+        synchronize: true,
+        autoLoadEntities: true
+      })
+    }),
     AuthModule
   ],
 })
---
+++ test/auth.e2e.spec.ts
@@ -0,0 +1,5 @@
+describe("Auth Module (e2e)", () => {
+  test("register: should return new user", () => {
+    expect(5).toEqual(5)
+  })
+})
